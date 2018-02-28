8d6b5df8dc57888592eb8208402fdc3842db1844
mpalm1234 <mpalm@udel.edu>
2018-02-28 17:36:48 -0500

    worked on background colors and smooth scrolling

--- global.js
+++ global.js
@@ -1,3 +1,24 @@
+// BACK TO TOP BUTTON
+function scrollNav() {
+  $('.nav a').click(function(){
+    //Toggle Class
+    $(".active").removeClass("active");
+    $(this).closest('li').addClass("active");
+
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+// BACK TO TOP BUTTON -- only make visible sometimes
 $(document).ready(function() {
   var btt = $('.back-to-top');
 
@@ -23,22 +44,32 @@ $(document).ready(function() {
   });
 });
 
+// Button smooth scrolling
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer)
+          return;
+        scrollContainer.scrollTop += 1;
+    }
+    while (scrollContainer.scrollTop == 0);
 
-function scrollNav() {
-  $('.nav a').click(function(){
-    //Toggle Class
-    $(".active").removeClass("active");
-    $(this).closest('li').addClass("active");
+    var targetY = 0;
+    do { //find the top of target relative to the container
+        if (target == scrollContainer)
+          break;
+        targetY += target.offsetTop;
+    }
+    while (target = target.offsetParent);
 
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 400);
-    return false;
-  });
-  $('.scrollTop a').scrollTop();
+    scroll = function(c, a, b, i) {
+        i++;
+        if (i > 30)
+          return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
-scrollNav();
