ea7fd7b4f5568b9f28830b9cd5db51bc0d3aadf5
mpalm1234 <mpalm@udel.edu>
2018-03-10 16:54:59 -0500

    adjusted a lot of javascript for smooth scrolling, changed it back to headings being cutoff if order to correct background color issues

--- global.js
+++ global.js
@@ -1,39 +1,42 @@
-// BACK TO TOP BUTTON
+
+
+// Navbar -- smooth scrolling
 function scrollNav() {
-  $('.nav a').click(function(){
-    //Toggle Class
-    $(".active").removeClass("active");
-    $(this).closest('li').addClass("active");
-
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 400);
-    return false;
+  $('.nav a').click(function(){             // apply to each button in navbar
+    $('html, body').animate({
+       scrollTop: $( $(this).attr('href') ).offset().top   // scroll to the href of button we clicked
+    }, 400);                                               // speed
   });
-  $('.scrollTop a').scrollTop();
 }
 scrollNav();
 
-// BACK TO TOP BUTTON -- only make visible sometimes
-$(document).ready(function() {
-  var btt = $('.back-to-top');
+// Patient/Clinician Buttons -- smooth scrolling
+function scrollButton() {
+  $('.whoAmI').click(function(){             // apply to each button in navbar
+    $('html, body').animate({
+       scrollTop: $( $(this).attr('href') ).offset().top   // scroll to the href of button we clicked
+    }, 400);                                               // speed
+  });
+}
+scrollButton();
 
+// Back To Top button
+$(document).ready(function() {
+
+  // smooth scrolling
+  let btt = $('.back-to-top');
   btt.on('click', function(e) {
     $('html, body').animate({
       scrollTop: 0
-    }, 500);
-
-    e.preventDefault();
+    }, 400);
+    e.preventDefault();   // default action of event will not be triggered
   });
 
+  // only make visible if scrolled down enough
   $(window).on('scroll', function() {
-    var self = $(this),
-        height = self.height(),   // height of screen (constant)
-        top = self.scrollTop();   // distance from top of screen to how far you are currently scrolled down
+    let self = $(this);
+    let height = self.height();   // height of screen (constant)
+    let top = self.scrollTop();   // distance from top of screen to how far you are currently scrolled down
     if(top > (height / 2)) {
       if(!btt.is('visible')) {
         btt.show();
@@ -43,33 +46,3 @@ $(document).ready(function() {
     }
   });
 });
-
-// Button smooth scrolling
-window.smoothScroll = function(target) {
-    var scrollContainer = target;
-    do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer)
-          return;
-        scrollContainer.scrollTop += 1;
-    }
-    while (scrollContainer.scrollTop == 0);
-
-    var targetY = 0;
-    do { //find the top of target relative to the container
-        if (target == scrollContainer)
-          break;
-        targetY += target.offsetTop;
-    }
-    while (target = target.offsetParent);
-
-    scroll = function(c, a, b, i) {
-        i++;
-        if (i > 30)
-          return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){scroll(c, a, b, i); }, 20);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
